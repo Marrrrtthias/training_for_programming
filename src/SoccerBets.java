68766f337d5b0d68a0609d4084ce4850d8b01d8a
Marrrrtthias <matthiasgellrich@gmail.com>
2017-04-18 19:53:19 +0200

    complete SoccerBets

--- src/SoccerBets.java
+++ src/SoccerBets.java
@@ -5,22 +5,41 @@ import java.util.*;
  */
 public class SoccerBets {
     public static void main(String[] args) {
-        Map<String, Boolean> teams = new HashMap<>();
-
         Scanner scanner = new Scanner(System.in);
         int numCases = scanner.nextInt();
-        scanner.nextLine();
 
-        for(int i = 0; i < 16; i++) {
-            String[] match = scanner.nextLine().split(" ");
-            if(Integer.parseInt(match[2])>Integer.parseInt(match[3]) && teams.get(match[0]) == null||teams.get(match[0])) {
-                // Team a hat gewonnen und ist noch unbekannt
-                // TODO
-            } else if(Integer.parseInt(match[2])<Integer.parseInt(match[3]) && (teams.get(match[1]) == null||teams.get(match[1]))) {
-                // Team b hat gewonnen und ist noch unbekannt oder nicht ausgeschieden
-                // TODO
+        for (int currentCase = 1; currentCase <= numCases; currentCase++) {
+            Set<String> knownTeams = new HashSet<>();       // all teams the program has come across so far
+            Set<String> winCandidates = new HashSet<>();    // all teams where no lost game has been found so far
+            scanner.nextLine();
+
+            for (int i = 0; i < 16; i++) {
+                String[] match = scanner.nextLine().split(" ");
+                if (Integer.parseInt(match[2]) > Integer.parseInt(match[3])) {
+                    // Team a hat gewonnen
+                    winCandidates.remove(match[1]);     // team b can not win anymore; remove it from winCandidates
+                    if(!knownTeams.contains(match[0])) {
+                        // only add team a to wincandidates if it was previously unknown, otherwhise it has already lost a game before
+                        winCandidates.add(match[0]);
+                    }
+                    knownTeams.add(match[0]);
+                    knownTeams.add(match[1]);
+                } else if (Integer.parseInt(match[2]) < Integer.parseInt(match[3])) {
+                    // Team b hat gewonnen
+                    winCandidates.remove(match[0]);     // team a can not win anymore; remove it from winCandidates
+                    if(!knownTeams.contains(match[1])) {
+                        winCandidates.add(match[1]);
+                    }
+                    knownTeams.add(match[0]);
+                    knownTeams.add(match[1]);
+                }
             }
-            // TODO
+
+            if(winCandidates.size() != 1) {     // sanity check
+                System.err.println("Something went wrong");
+                System.exit(1);
+            }
+            System.out.println("Case #" + currentCase + ": " + winCandidates.toArray()[0]);
         }
     }
 }
