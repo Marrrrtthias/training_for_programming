3d1d406daadf6c63895bfcae819fb9b6e52ad0c8
Matthias Gellrich <matthiasgellrich@gmail.com>
2017-05-02 20:25:18 +0200

    Dwarves are fucked

--- src/Dwarves.java
+++ src/Dwarves.java
@@ -4,40 +4,69 @@ import java.util.*;
  * Created by matthias on 5/2/17.
  */
 public class Dwarves {
+    static Map<String, Collection<String>> dwarfMap = new HashMap<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numDwarves = scanner.nextInt();
+        int numCases = scanner.nextInt();
         scanner.nextLine();
 
-        Map<String, LinkedList<String>> dwarfGraph = new HashMap<>();
-        String currentDwarf;
 
-        // gerichteten graph aufbauen
-        for (int currCase = 1; currCase <= numDwarves; currCase++) {
+        for (int currCase = 1; currCase <= numCases; currCase++) {
             String[] line = scanner.nextLine().split(" ");
-            if (line[1].equals(">")) {
-                if (!dwarfGraph.keySet().contains(line[0])) {
-                    dwarfGraph.put(line[0], new LinkedList<>());
-                }
-                dwarfGraph.get(line[0]).add(line[2]);
-            } else if (line[1].equals("<")) {
-                if (!dwarfGraph.keySet().contains(line[2])) {
-                    dwarfGraph.put(line[2], new LinkedList<>());
-                }
-                dwarfGraph.get(line[2]).add(line[0]);
+            String[] relation;
+            if (line[1].equals("<")) {
+                relation = new String[2];
+                relation[0] = line[2];
+                relation[1] = line[0];
+            } else if (line[1].equals(">")) {
+                relation = new String[2];
+                relation[0] = line[0];
+                relation[1] = line[2];
+            } else {
+                throw new InputMismatchException();
             }
+            dwarfMap.putIfAbsent(relation[0], new HashSet<>());
+            dwarfMap.putIfAbsent(relation[1], new HashSet<>());
+            if (dwarfMap.get(relation[1]).contains(relation[0]) || relation[0].equals(relation[1])) {
+                System.out.println("impossible");
+                System.exit(0);
+            }
+            dwarfMap.get(relation[0]).add(relation[1]);
         }
 
-        // Zykel suchen
-        boolean hasCycle = false;
-        Set<String> visited = new HashSet<>();
-        LinkedList<String> remaining = new LinkedList<>();
-        remaining.addAll(dwarfGraph.keySet());
-        while (!remaining.isEmpty()) {
-            String current = remaining.remove();
+        for (String root : dwarfMap.keySet()) {
+            Map<String, Boolean> visited = new HashMap<>();
+            Map<String, Boolean> finished = new HashMap<>();
+            Map<String, List<String>> backStack = new HashMap<>();
+            String current = null;
+            Stack<String> todo = new Stack<>();
+            todo.push(root);
+
+            while (!todo.isEmpty()) {
+                if (current != null) {
+                    backStack.putIfAbsent(todo.peek(), new LinkedList<>());
+                    backStack.get(todo.peek()).add(current);
+                }
+                current = todo.pop();
+                finished.putIfAbsent(current, false);
+
+                if (finished.get(current)) {
+                    continue;
+                }
+                backStack.putIfAbsent(current, new LinkedList<>());
+                if (backStack.get(current).contains(current)) {
+                    System.out.println("impossible");
+                    System.exit(0);
+                }
+                for (String neighbor : dwarfMap.get(current)) {
+                    todo.push(neighbor);
+                }
+                finished.put(current, true);
+            }
             // TODO
         }
 
-        System.out.println(/*TODO print something*/);
+        System.out.println("possible");
     }
 }
