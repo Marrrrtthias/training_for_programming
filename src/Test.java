d8951c1f9ef70d7144bdb59cd8ea6c675978b5b8
Matthias Gellrich <matthiasgellrich@gmail.com>
2017-05-20 12:47:19 +0200

    add Test-generator

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by matthias on 5/16/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(10000);
+        for (int i = 1; i <= 10000; i++) {
+            System.out.println(i + " " + i);
+        }
+    }
+}
