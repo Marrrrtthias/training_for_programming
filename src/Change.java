f04764ab6d5dae5fc0556881c18159fb1ccccd77
Matthias Gellrich <matthiasgellrich@gmail.com>
2017-05-30 15:32:44 +0200

    finish Making Change (except timelimit)

--- src/Change.java
+++ src/Change.java
@@ -1,10 +1,16 @@
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  * Created by matthias on 5/30/17.
  */
 public class Change {
-    public static void main(String[] args) {
+    static int[][] coinsToUse;
+    static Map<Integer,Integer> coinToIndex;
+
+    public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
         int numCases = scanner.nextInt();
 
@@ -15,23 +21,46 @@ public class Change {
             int toPay = Integer.parseInt(input[1]);
 
             int[] coinValues = new int[numCoins];
+            coinsToUse = new int[toPay+1][numCoins];
+            coinToIndex = new TreeMap<>();
+            int[] bestSoFar = new int[toPay+1]; for (int i = 0; i < bestSoFar.length; i++) bestSoFar[i] = Integer.MAX_VALUE;
+            bestSoFar[0] = 0;
             for (int i = 0; i < coinValues.length; i++) {
                 coinValues[i] = scanner.nextInt();
-            }
-            scanner.nextLine();
-
-            int[] coinsToUse = new int[numCoins];
-            int currValue = 0;
-            for (int i = coinValues.length-1; i >= 0; i--) {
-                while (currValue + coinValues[i] <= toPay) {
-                    coinsToUse[i]++;
-                    currValue += coinValues[i];
+                coinToIndex.put(coinValues[i], i);
+                if (coinValues[i] < coinsToUse.length) {
+                    coinsToUse[coinValues[i]][i] = 1;
+                    bestSoFar[coinValues[i]] = 1;
                 }
-                if (currValue == toPay)
-                    break;
             }
 
-            System.out.println("Case #" + currCase + ":" + arrayToString(coinsToUse));
+            for (int i = 1; i < coinsToUse.length; i++) {
+                //System.out.println("building from value: " + i);
+                for (int coinValue : coinValues) {
+                    int valueBeingBuilt = i + coinValue;
+                    //System.out.println("building value: " + valueBeingBuilt);
+                    if (valueBeingBuilt > toPay || bestSoFar[valueBeingBuilt] < (bestSoFar[i]+1)) continue;
+
+                    copyCoins(i, valueBeingBuilt);
+                    coinsToUse[valueBeingBuilt][coinToIndex.get(coinValue)] += 1;
+                    bestSoFar[valueBeingBuilt] = bestSoFar[i] + 1;
+                }
+            }
+
+            System.out.println("Case #" + currCase + ":" + arrayToString(coinsToUse[toPay]));
+            //System.out.println(Arrays.toString(bestSoFar));
+
+            try {
+                scanner.nextLine();
+            } catch (Exception e) {
+                // dont care
+            }
+        }
+    }
+
+    private static void copyCoins(int a, int b) {
+        for (int i = 0; i < coinsToUse[a].length; i++) {
+            coinsToUse[b][i] = coinsToUse[a][i];
         }
     }
 
